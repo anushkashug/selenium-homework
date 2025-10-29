4339e502e955a8a07fe365d66b2d8258a3a7fc81
ana shughliashvili <ana@gmail.com>
2025-10-29 14:53:29 +0400

    Initial commit: Selenium homework project

---
+++ src/test/java/CommandsTest.java
@@ -0,0 +1,72 @@
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
+
+public class CommandsTest {
+public static void main(String[] args) {
+    WebDriver driver = new ChromeDriver();
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(25));
+
+    driver.manage().window().maximize();
+
+    driver.get("http://the-internet.herokuapp.com/dynamic_controls");
+
+    System.out.println("Page opened successfully!");
+
+
+
+    WebElement enableButton = driver.findElement(By.xpath("//button[text()='Enable']"));
+    enableButton.click();
+    try { Thread.sleep(2000); } catch (InterruptedException e) { e.printStackTrace(); }
+    wait.until(ExpectedConditions.textToBePresentInElement(enableButton, "Disable"));
+    String buttonText = enableButton.getText();git init
+
+
+    if (buttonText.equals("Disable")) {
+        System.out.println(" Button text changed ");
+    } else {
+        System.out.println(" Button text did not change" + buttonText);
+    }
+    try { Thread.sleep(2000); } catch (InterruptedException e) { e.printStackTrace(); }
+
+
+
+
+    WebElement inputField = driver.findElement(By.xpath("//input[@type='text']"));
+
+    inputField.sendKeys("Bootcamp");
+    System.out.println("Text 'Bootcamp' entered in the input field.");
+
+    try { Thread.sleep(2000); } catch (InterruptedException e) { e.printStackTrace(); }
+
+    inputField.clear();
+
+
+    try { Thread.sleep(2000); } catch (InterruptedException e) { e.printStackTrace(); }
+
+
+    driver.get("http://the-internet.herokuapp.com/drag_and_drop");
+    try { Thread.sleep(2000); } catch (InterruptedException e) { e.printStackTrace(); }
+
+
+    WebElement columnA = driver.findElement(By.id("column-a"));
+    WebElement columnB = driver.findElement(By.id("column-b"));
+
+
+    int yA = columnA.getLocation().getY();
+    int yB = columnB.getLocation().getY();
+    if (yA == yB) {
+        System.out.println(" Columns A and B aligned ");
+    } else {
+        System.out.println(" Columns not aligned (A: " + yA + ", B: " + yB + ")");
+    }
+    try { Thread.sleep(2000); } catch (InterruptedException e) { e.printStackTrace(); }
+
+}
+}
